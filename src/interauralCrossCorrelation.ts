f342e9f6287a6a1e6361bfafbf3f8158da351972
Paul Schwörer <hello@paulschwoerer.de>
2022-10-24 12:27:56 +0200

    fix: correct calculation for early iacc

--- src/interauralCrossCorrelation.ts
+++ src/interauralCrossCorrelation.ts
@@ -1,18 +1,24 @@
-import { elf } from "./elf";
-import { arrayMax } from "./math/arrayMax";
-import { normalizeArray } from "./math/normalizeArray";
-import { xcorr } from "./xcorr";
+/* eslint-disable import/extensions */
+import { elf } from './elf';
+import { arrayMax } from './math/arrayMax';
+import { normalizeArray } from './math/normalizeArray';
+import { xcorr } from './xcorr';
 
 /**
  * Performs interaural cross correlation on two channels, which previously have been separated into their octave bands.
- * 
+ *
  * @param leftBands Octave bands of left channel
  * @param rightBands Octave bands of right channel
- * @returns 
+ * @returns
  */
-export async function interauralCrossCorrelation(leftBands: Float64Array[], rightBands: Float64Array[]): Promise<Float64Array> {
+export async function interauralCrossCorrelation(
+  leftBands: Float64Array[],
+  rightBands: Float64Array[]
+): Promise<Float64Array> {
   if (leftBands.length !== rightBands.length) {
-    throw new Error("expected left and right channel to have the same number of octave bands")
+    throw new Error(
+      'expected left and right channel to have the same number of octave bands'
+    );
   }
 
   const numOctaveBands = leftBands.length;
@@ -24,11 +30,11 @@ export async function interauralCrossCorrelation(leftBands: Float64Array[], righ
 
     const leftSum = leftChannel.reduce((prev, cur) => prev + cur ** 2);
     const rightSum = rightChannel.reduce((prev, cur) => prev + cur ** 2);
-    
-    const crossCorrelated = await xcorr(leftChannel, rightChannel);
-    
+
+    const crossCorrelated = xcorr(leftChannel, rightChannel);
+
     const normalized = normalizeArray(
-      crossCorrelated, 
+      crossCorrelated,
       Math.sqrt(leftSum * rightSum)
     );
 
@@ -38,9 +44,15 @@ export async function interauralCrossCorrelation(leftBands: Float64Array[], righ
   return res;
 }
 
-export async function earlyInterauralCrossCorrelation(leftBands: Float64Array[], rightBands: Float64Array[], fs: number) {
+export async function earlyInterauralCrossCorrelation(
+  leftBands: Float64Array[],
+  rightBands: Float64Array[],
+  fs: number
+) {
   if (leftBands.length !== rightBands.length) {
-    throw new Error("expected left and right channel to have the same number of octave bands")
+    throw new Error(
+      'expected left and right channel to have the same number of octave bands'
+    );
   }
 
   const numOctaveBands = leftBands.length;
@@ -51,19 +63,18 @@ export async function earlyInterauralCrossCorrelation(leftBands: Float64Array[],
 
     const { e80: e80Left } = elf(leftChannel, fs);
     const { e80: e80Right } = elf(rightChannel, fs);
-    
+
     const leftSum = e80Left.reduce((prev, cur) => prev + cur ** 2);
     const rightSum = e80Right.reduce((prev, cur) => prev + cur ** 2);
-    
-    const crossCorrelated = await xcorr(e80Left, e80Right);
-    
+
+    const crossCorrelated = xcorr(e80Left, e80Right);
+
     const normalized = normalizeArray(
-      crossCorrelated, 
+      crossCorrelated,
       Math.sqrt(leftSum * rightSum)
     );
 
-    // TODO: understand why *-1 + 1 fixes the curve
-    res[i] = ((1 - arrayMax(normalized)) * -1) + 1;
+    res[i] = arrayMax(normalized);
   }
 
   return res;
