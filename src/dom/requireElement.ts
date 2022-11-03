5e41281d58f7278cec34c7f6054d65fccb9bb6bd
Paul Schwörer <hello@paulschwoerer.de>
2022-11-03 22:47:32 +0100

    chore: remove unused function

--- src/dom/requireElement.ts
+++
@@ -1,9 +0,0 @@
-export function requireElement<T extends HTMLElement>(id: string): T {
-  const el = document.getElementById(id);
-
-  if (el === null) {
-    throw new Error(`element with id ${id} not found`);
-  }
-
-  return el as T;
-}
