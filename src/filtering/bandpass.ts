662d08499cbdd2bcaaf3c0df4eb7746c91e2dc81
Paul Schwörer <hello@paulschwoerer.de>
2022-11-06 18:12:58 +0100

    refactor: optimize bandpass function

--- src/filtering/bandpass.ts
+++ src/filtering/bandpass.ts
@@ -23,13 +23,13 @@ type Coefficients = {
  * @param order
  * @param f1
  * @param f2
- * @param fs
+ * @param sampleRate
  */
 export function bandpass(
   order: number,
   f1: number,
   f2: number,
-  fs: number
+  sampleRate: number
 ): Coefficients {
   if (order % 2 !== 0) {
     throw new Error('only filters with an even order are supported');
@@ -40,59 +40,45 @@ export function bandpass(
   const numberOfBiquads = order / 2;
 
   // continuous frequency variables
-  const F1 = (fs / Math.PI) * Math.tan((Math.PI * f1) / fs);
-  const F2 = (fs / Math.PI) * Math.tan((Math.PI * f2) / fs);
+  const F1 = (sampleRate / Math.PI) * Math.tan((Math.PI * f1) / sampleRate);
+  const F2 = (sampleRate / Math.PI) * Math.tan((Math.PI * f2) / sampleRate);
   const BW = F2 - F1; // -3dB bandwith in Hz
-  const F0 = Math.sqrt(F1 * F2); // geometric mean frequency in Hz
+  const f0 = Math.sqrt(f1 * f2);
+  const F0 = Math.sqrt(F1 * F2);
 
-  const k = [];
-  const theta = [];
-  const pLp = [];
-
-  for (let i = 0; i < numberOfBiquads; i += 1) {
-    k[i] = i + 1;
-    theta[i] = ((2 * k[i] - 1) * Math.PI) / (2 * numberOfBiquads);
-    pLp[i] = new Complex(-Math.sin(theta[i]), Math.cos(theta[i]));
-  }
-
-  const pa = [];
-  const p = [];
+  const b = [1, 0, -1];
+  const feedbacks = new Array(numberOfBiquads);
+  const gains = new Array(numberOfBiquads);
 
   for (let i = 0; i < numberOfBiquads; i += 1) {
-    const alpha = multiply((BW / F0) * 0.5, pLp[i]);
+    const k = i + 1;
+    const theta = ((2 * k - 1) * Math.PI) / (2 * numberOfBiquads);
+
+    const pLp = new Complex(-Math.sin(theta), Math.cos(theta));
+
+    const alpha = multiply((BW / F0) * 0.5, pLp);
     const beta = sqrt(
-      subtract(new Complex(1, 0), pow(multiply((BW / F0) * 0.5, pLp[i]), 2))
+      subtract(new Complex(1, 0), pow(multiply((BW / F0) * 0.5, pLp), 2))
     );
-    pa[i] = multiply(
+    const pa = multiply(
       2 * Math.PI * F0,
       add(alpha, multiply(beta, new Complex(0, 1)))
     );
-    p[i] = divide(
-      add(new Complex(1, 0), divide(pa[i], 2 * fs)),
-      subtract(new Complex(1, 0), divide(pa[i], 2 * fs))
+    const p = divide(
+      add(new Complex(1, 0), divide(pa, 2 * sampleRate)),
+      subtract(new Complex(1, 0), divide(pa, 2 * sampleRate))
     );
-  }
 
-  const a = [];
+    const a1 = -2 * p.getRe();
+    const a2 = absComplex(p) ** 2;
+    feedbacks[i] = [1, a1, a2];
 
-  for (let i = 0; i < numberOfBiquads; i += 1) {
-    const a1 = -2 * p[i].getRe();
-    const a2 = absComplex(p[i]) ** 2;
-    a[i] = [1, a1, a2];
-  }
-
-  const b = [1, 0, -1];
-  const f0 = Math.sqrt(f1 * f2);
-  const K = [];
-
-  for (let i = 0; i < numberOfBiquads; i += 1) {
-    const aa = a[i];
-    const h = frequencyResponse(b, aa, [f0, f0], fs);
-    K[i] = 1 / absComplex(h[0]);
+    const h = frequencyResponse(b, feedbacks[i], [f0, f0], sampleRate);
+    gains[i] = 1 / absComplex(h[0]);
   }
 
   return {
-    feedbacks: a,
-    gains: K,
+    feedbacks,
+    gains,
   };
 }
