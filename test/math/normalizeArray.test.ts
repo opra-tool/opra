30644215f9d497304895b69304db9b0098128ae8
Paul Schwörer <hello@paulschwoerer.de>
2022-12-11 20:45:13 +0100

    refactor: remove unused function

--- test/math/normalizeArray.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from '@esm-bundle/chai';
-import { normalizeArray } from '../../src/math/normalizeArray';
-
-it('should normalize the array', () => {
-  const actual = normalizeArray(new Float64Array([4, 2, 1.87, 2.04]), 2);
-
-  expect(actual).to.deep.equal(new Float64Array([2, 1, 0.935, 1.02]));
-});
