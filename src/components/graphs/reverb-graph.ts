9e90964e036541541cea2cdfc3579090f46fac53
Paul Schwörer <hello@paulschwoerer.de>
2023-02-19 12:38:51 +0100

    feat: "energy decay curve" -> "energy decay time"

--- src/components/graphs/reverb-graph.ts
+++ src/components/graphs/reverb-graph.ts
@@ -21,7 +21,7 @@ export class ReverbGraph extends LitElement {
     const params = [
       {
         key: 'edt',
-        label: msg('Energy Decay Curve'),
+        label: msg('Energy Decay Time'),
         datasets: this.edt.map((values, index) => ({
           color: this.impulseResponses[index].color,
           values,
