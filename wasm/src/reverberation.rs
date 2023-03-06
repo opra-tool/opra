ecaa250c8c8bf5298a0328ab327e4bc63a17a511
Paul Schwörer <hello@paulschwoerer.de>
2023-03-06 19:24:19 +0100

    refactor: rename variables

--- wasm/src/reverberation.rs
+++ wasm/src/reverberation.rs
@@ -4,7 +4,8 @@ use crate::linear_algebra::linear_function_steepness;
 #[wasm_bindgen]
 #[allow(dead_code)]
 pub fn calculate_reverberation(squared_ir: Vec<f32>, tc: f32, fs: f32) -> Vec<f32> {
-  let sum = squared_ir.iter().sum::<f32>();
+  let total_energy = squared_ir.iter().sum::<f32>();
+
   let cumulative_sum: Vec<f32> = squared_ir
     .into_iter()
     .scan(0.0, |acc, x| {
@@ -13,25 +14,25 @@ pub fn calculate_reverberation(squared_ir: Vec<f32>, tc: f32, fs: f32) -> Vec<f3
     })
     .collect();
 
-  let si1: Vec<f32> = cumulative_sum
+  let backward_integration: Vec<f32> = cumulative_sum
     .into_iter()
-    .map(|x| (sum - x))
+    .map(|x| (total_energy - x))
     .collect();
 
-  let max_si1 = si1.iter()
+  let max_backward_integration = backward_integration.iter()
     .max_by(|a, b| a.partial_cmp(b).unwrap())
     .unwrap();
 
-  let ec = si1
+  let decay_curve = backward_integration
     .iter()
-    .map(|val| 10.0 * (val / max_si1).log10())
+    .map(|val| 10.0 * (val / max_backward_integration).log10())
     .collect();
 
-  return vec![calc(&ec, -10.0, 0.0, fs), calc(&ec, -(5.0 + tc), -5.0, fs)];
+  return vec![extrapolate_curve(&decay_curve, -10.0, 0.0, fs), extrapolate_curve(&decay_curve, -(5.0 + tc), -5.0, fs)];
 }
 
-fn calc(ec: &Vec<f32>, min: f32, max: f32, fs: f32) -> f32 {
-  let y_values: Vec<f32> = ec
+fn extrapolate_curve(decay_curve: &Vec<f32>, min: f32, max: f32, fs: f32) -> f32 {
+  let y_values: Vec<f32> = decay_curve
     .iter()
     .filter(|val| (**val > min && **val < max))
     .map(|x| *x)
